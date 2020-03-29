9c27f6f841c9c54a839a7e687dcbcd3a116e25ac
JB Wilson <53917177+Selenestica@users.noreply.github.com>
2020-03-29 16:33:03 -0500

    turns out the problem was i didn't rename the html file

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 
 // COMPONENT IMPORTS
-import Navbar from "./components/Navbar";
 import Order from "./components/Order";
 import OrderSuccess from "./components/OrderSuccess";
 import About from "./components/About";
 import Landing from "./components/Landing";
+import Layout from "./components/Layout";
 
 // CSS IMPORTS
 import "./css/index.css";
@@ -20,15 +20,14 @@ import "./css/DrinksMenu.css";
 import "./css/About.css";
 import "./css/App.css";
 import "materialize-css/dist/css/materialize.min.css";
-import Layout from "./components/Layout";
 
 class App extends Component {
   render() {
     let routes = (
       <Switch>
-        <Route exact path="/order-details" component={Order} />
-        <Route exact path="/order-success" component={OrderSuccess} />
-        <Route exact path="/about" component={About} />
+        <Route path="/order-details" component={Order} />
+        <Route path="/order-success" component={OrderSuccess} />
+        <Route path="/about" component={About} />
         <Route exact path="/" component={Landing} />
       </Switch>
     );
