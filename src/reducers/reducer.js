9eb0e0ff1cce0d0e3a46ac21903f9b4920ca4822
JB Wilson <53917177+Selenestica@users.noreply.github.com>
2020-03-30 22:58:25 -0500

    fixed bug where the input validation was broken, not letting you place an order

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,3 +1,5 @@
+import { createAction } from "@reduxjs/toolkit";
+
 const initialState = {
   customerAddress: "",
   addressNotNull: false,
@@ -23,150 +25,167 @@ const initialState = {
   missingField: false
 };
 
-const reducer = (state = initialState, action) => {
-  if (action.type === "ADDRESS_SAVED") {
-    return {
-      ...state,
-      customerAddress: action.customerInfo,
-      addressNotNull: true
-    };
-  } else if (action.type === "CUSTOMER_CONTACT") {
-    return {
-      ...state,
-      customerEmail: action.customerEmail,
-      customerPhone: action.customerPhone,
-      customerFirst: action.customerFirst,
-      customerLast: action.customerLast
-    };
-  } else if (action.type === "STORE_INFO_SAVED") {
-    return {
-      ...state,
-      storeId: action.storeId,
-      storeIdNotNull: true,
-      storeAddress: action.storeAddress,
-      storePhone: action.storePhone
-    };
-  } else if (action.type === "SAVE_ITEM") {
-    return {
-      ...state,
-      itemId: state.itemId.concat(action.itemId),
-      itemName: state.itemName.concat(action.itemName + ", ")
-    };
-  } else if (action.type === "CLEAR_ORDER") {
-    return {
-      ...state,
-      itemId: [],
-      itemName: []
-    };
-  } else if (action.type === "OPEN_DRINKS_MENU") {
-    if (state.drinksMenuOpen === false) {
-      return {
-        ...state,
-        drinksMenuOpen: true
-      };
-    } else {
-      return {
-        ...state,
-        drinksMenuOpen: false
-      };
-    }
-  } else if (action.type === "OPEN_PIZZA_MENU") {
-    if (state.pizzaMenuOpen === false) {
-      return {
-        ...state,
-        pizzaMenuOpen: true
-      };
-    } else {
-      return {
-        ...state,
-        pizzaMenuOpen: false
-      };
-    }
-  } else if (action.type === "OPEN_BREAD_MENU") {
-    if (state.breadMenuOpen === false) {
-      return {
-        ...state,
-        breadMenuOpen: true
-      };
-    } else {
-      return {
-        ...state,
-        breadMenuOpen: false
-      };
-    }
-  } else if (action.type === "OPEN_DESSERT_MENU") {
-    if (state.dessertMenuOpen === false) {
-      return {
-        ...state,
-        dessertMenuOpen: true
-      };
-    } else {
-      return {
-        ...state,
-        dessertMenuOpen: false
-      };
-    }
-  } else if (action.type === "OPEN_PASTA_MENU") {
-    if (state.pastaMenuOpen === false) {
-      return {
-        ...state,
-        pastaMenuOpen: true
-      };
-    } else {
-      return {
-        ...state,
-        pastaMenuOpen: false
-      };
-    }
-  } else if (action.type === "OPEN_SANDWICH_MENU") {
-    if (state.sandwichMenuOpen === false) {
-      return {
-        ...state,
-        sandwichMenuOpen: true
-      };
-    } else {
-      return {
-        ...state,
-        sandwichMenuOpen: false
-      };
-    }
-  } else if (action.type === "OPEN_SALAD_MENU") {
-    if (state.saladMenuOpen === false) {
-      return {
-        ...state,
-        saladMenuOpen: true
-      };
-    } else {
-      return {
-        ...state,
-        saladMenuOpen: false
-      };
-    }
-  } else if (action.type === "OPEN_WINGS_MENU") {
-    if (state.wingsMenuOpen === false) {
-      return {
-        ...state,
-        wingsMenuOpen: true
-      };
-    } else {
-      return {
-        ...state,
-        wingsMenuOpen: false
-      };
-    }
-  } else if (action.type === "DISPLAY_ORDER_ERROR") {
-    return {
-      ...state,
-      isError: true
-    };
-  } else if (action.type === "DISPLAY_MISSING_FIELD_ERROR") {
-    return {
-      ...state,
-      missingField: true
-    };
-  }
+const ADDRESS_SAVED = createAction("ADDRESS_SAVED");
+const CUSTOMER_CONTACT = createAction("CUSTOMER_CONTACT");
+const STORE_INFO_SAVED = createAction("STORE_INFO_SAVED");
+const SAVE_ITEM = createAction("SAVE_ITEM");
+const CLEAR_ORDER = createAction("CLEAR_ORDER");
+const OPEN_DRINKS_MENU = createAction("OPEN_DRINKS_MENU");
+const OPEN_PIZZA_MENU = createAction("OPEN_PIZZA_MENU");
+const OPEN_BREAD_MENU = createAction("OPEN_BREAD_MENU");
+const OPEN_DESSERT_MENU = createAction("OPEN_DESSERT_MENU");
+const OPEN_PASTA_MENU = createAction("OPEN_PASTA_MENU");
+const OPEN_SANDWICH_MENU = createAction("OPEN_SANDWICH_MENU");
+const OPEN_SALAD_MENU = createAction("OPEN_SALAD_MENU");
+const OPEN_WINGS_MENU = createAction("OPEN_WINGS_MENU");
+const DISPLAY_OROER_ERROR = createAction("DISPLAY_ORDER_ERROR");
+const DISPLAY_MISSING_FIELD_ERROR = createAction("DISPLAY_MISSING_FIELD_ERROR");
 
-  return state;
-};
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case ADDRESS_SAVED.type:
+      return {
+        ...state,
+        customerAddress: action.customerInfo,
+        addressNotNull: true
+      };
+    case CUSTOMER_CONTACT.type:
+      return {
+        ...state,
+        customerEmail: action.customerEmail,
+        customerPhone: action.customerPhone,
+        customerFirst: action.customerFirst,
+        customerLast: action.customerLast
+      };
+    case STORE_INFO_SAVED.type:
+      return {
+        ...state,
+        storeId: action.storeId,
+        storeIdNotNull: true,
+        storeAddress: action.storeAddress,
+        storePhone: action.storePhone
+      };
+    case SAVE_ITEM.type:
+      return {
+        ...state,
+        itemId: state.itemId.concat(action.itemId),
+        itemName: state.itemName.concat(action.itemName + ", ")
+      };
+    case CLEAR_ORDER.type:
+      return {
+        ...state,
+        itemId: [],
+        itemName: []
+      };
+    case OPEN_DRINKS_MENU.type:
+      if (state.drinksMenuOpen === false) {
+        return {
+          ...state,
+          drinksMenuOpen: true
+        };
+      } else {
+        return {
+          ...state,
+          drinksMenuOpen: false
+        };
+      }
+    case OPEN_PIZZA_MENU.type:
+      if (state.pizzaMenuOpen === false) {
+        return {
+          ...state,
+          pizzaMenuOpen: true
+        };
+      } else {
+        return {
+          ...state,
+          pizzaMenuOpen: false
+        };
+      }
+    case OPEN_BREAD_MENU.type:
+      if (state.breadMenuOpen === false) {
+        return {
+          ...state,
+          breadMenuOpen: true
+        };
+      } else {
+        return {
+          ...state,
+          breadMenuOpen: false
+        };
+      }
+    case OPEN_DESSERT_MENU.type:
+      if (state.dessertMenuOpen === false) {
+        return {
+          ...state,
+          dessertMenuOpen: true
+        };
+      } else {
+        return {
+          ...state,
+          dessertMenuOpen: false
+        };
+      }
+    case OPEN_PASTA_MENU.type:
+      if (state.pastaMenuOpen === false) {
+        return {
+          ...state,
+          pastaMenuOpen: true
+        };
+      } else {
+        return {
+          ...state,
+          pastaMenuOpen: false
+        };
+      }
+    case OPEN_SANDWICH_MENU.type:
+      if (state.sandwichMenuOpen === false) {
+        return {
+          ...state,
+          sandwichMenuOpen: true
+        };
+      } else {
+        return {
+          ...state,
+          sandwichMenuOpen: false
+        };
+      }
+    case OPEN_SALAD_MENU.type:
+      if (state.saladMenuOpen === false) {
+        return {
+          ...state,
+          saladMenuOpen: true
+        };
+      } else {
+        return {
+          ...state,
+          saladMenuOpen: false
+        };
+      }
+    case OPEN_WINGS_MENU.type:
+      if (state.wingsMenuOpen === false) {
+        return {
+          ...state,
+          wingsMenuOpen: true
+        };
+      } else {
+        return {
+          ...state,
+          wingsMenuOpen: false
+        };
+      }
+    case DISPLAY_OROER_ERROR.type:
+      return {
+        ...state,
+        isError: true
+      };
+    case DISPLAY_MISSING_FIELD_ERROR.type:
+      return {
+        ...state,
+        missingField: true
+      };
+    default:
+      return state;
+  }
+}
 
 export default reducer;
